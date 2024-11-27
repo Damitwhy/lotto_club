c5ffb963948af3d5eb190245a4026d55d60e2895
Ken Cox <159484600+Damitwhy@users.noreply.github.com>
2024-11-27 17:08:26 +0000

    slash on STATIC_URL taken out leaving trailing slash

--- project_root/settings.py
+++ project_root/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = '/static/' #ALLREADY DOWN BELOW
+STATIC_URL = 'static/' #ALLREADY DOWN BELOW
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
